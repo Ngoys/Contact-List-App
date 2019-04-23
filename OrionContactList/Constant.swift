8244d56f8b656ebfaec6d205163adff1dd05f695
Yen Sern <yensern95@gmail.com>
2019-04-23 22:34:34 +0800

    setting up VC parent + data.json+ Constant

---
+++ OrionContactList/Constant.swift
@@ -0,0 +1,33 @@
+//
+//  Constant.swift
+//  OrionContactList
+//
+//  Created by Ngo Yen Sern on 23/04/2019.
+//  Copyright © 2019 ngoys. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct TextFile {
+        static let name = "data"
+        static let type = "json"
+        
+        static let fullFileName = name + "." + type
+        
+        static let commonErrorMessage = "Something wrong happened, please report it to our customer service."
+    }
+    
+    struct Text {
+        static let done = "Done!"
+        static let error = "Error"
+        static let noResult = "No Result Found"
+    }
+}
+
+struct Status {
+    static let success = "OK"
+    static let error = "error"
+}
+
---
+++ OrionContactList/VC_ContactDetail.swift
@@ -0,0 +1,12 @@
+//
+//  VC_ContactDetail.swift
+//  OrionContactList
+//
+//  Created by Ngo Yen Sern on 23/04/2019.
+//  Copyright © 2019 ngoys. All rights reserved.
+//
+
+import Foundation
+
+class VC_ContactDetail: VC_Base {
+}
