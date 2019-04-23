d52f1a03a4b0634b7cd9bf4c4b2946c37f6c74c2
Yen Sern <yensern95@gmail.com>
2019-04-23 22:29:33 +0800

    Initial Commit

---
+++ OrionContactList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OrionContactList
+//
+//  Created by Ngo Yen Sern on 23/04/2019.
+//  Copyright © 2019 ngoys. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
